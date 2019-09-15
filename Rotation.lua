a4ace8f45d5085b679dc1fb246e84cf423f1886b
Panglo <john.l.roste@gmail.com>
2019-09-15 10:59:52 -0600

    Priest stuff and things
    Bunch of updates:
    Holy Fire, renew count, buff toggles, Auto Target improvements, Mindblast snipe?, Documentation

--- Rotation.lua
+++ Rotation.lua
@@ -5,6 +5,10 @@ local Rotation = DMW.Helpers.Rotation
 local Setting = DMW.Helpers.Rotation.Setting
 local ShootTime = GetTime()
 
+
+--------------
+----Locals----
+--------------
 local function Locals()
     Player = DMW.Player
     Buff = Player.Buffs
@@ -30,6 +34,10 @@ local function Locals()
     end
 end
 
+
+----------------
+--Smart Recast--
+----------------
 local function smartRecast(spell,unit)
     if (not Spell[spell]:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or 
         not UnitIsUnit(Spell[spell].LastBotTarget, unit.Pointer)) then 
@@ -37,6 +45,10 @@ local function smartRecast(spell,unit)
     end
 end
 
+
+--------------
+--5 Sec Rule--
+--------------
 local function FiveSecond()
     if FiveSecondRuleTime == nil then
         FiveSecondRuleTime = DMW.Time 
@@ -45,12 +57,16 @@ local function FiveSecond()
     if FiveSecondRuleCount > 5 then
         FiveSecondRuleTime = DMW.Time 
     end
-    if Setting("Five Second Rule") and (FiveSecondRuleCount) >= Setting("Five Second Cutoff") then return true end
+    if Setting("Five Second Rule") and ((FiveSecondRuleCount) >= Setting("Five Second Cutoff") or (FiveSecondRuleCount <= 0.75)) then return true end
     --print(FiveSecondRuleCount)
 end
 
+---------------
+----Healing----
+---------------
 local function HEAL()
     if Friends40YC >= 1 then
+        -- Fort Buff on Party
         if Setting("Fort Buff Spread") then
             for _, Friend in ipairs(Friends40Y) do
                 if not Buff.PowerWordFortitude:Exist(Friend) then
@@ -58,27 +74,37 @@ local function HEAL()
                 end
             end 
         end
+        -- Cycle Party HP Values
         for _, Friend in ipairs(Friends40Y) do
+            --Party Renew
+            if Setting("Party - Renew") and Friend.HP < Setting("Party - Renew Percent") and not Buff.Renew:Exist(Friend) and Friend:IsTanking() and Buff.Renew:Count() < Setting("Renew Count Limit") then
+                if smartRecast("Renew",Friend) then FiveSecondRuleTime = DMW.Time return true end
+            end
+            -- Party Heal
             if Setting("Party - Heal") and Friend.HP < Setting("Party - Heal Percent") then
                 if smartRecast("Heal",Friend) then FiveSecondRuleTime = DMW.Time return true end
             end
+            -- Party Flash Heal
             if Setting("Party - Flash Heal") and Spell.FlashHeal:IsReady() and Friend.HP < Setting("Party - Flash Heal Percent") then
                 if smartRecast("FlashHeal",Friend) then FiveSecondRuleTime = DMW.Time return true end
             end
+            -- Party Lesser Heal
             if Setting("Party - Lesser Heal") and Spell.LesserHeal:IsReady() and Friend.HP <= Setting("Party - Lesser Heal Percent") then
                 if smartRecast("LesserHeal",Friend) then FiveSecondRuleTime = DMW.Time return true end
             end
+            -- Party Shield
             if Setting("Party - Shield") and Friend.HP < Setting("Party - Shield Percent") and not Buff.PowerWordShield:Exist(Friend) and not Debuff.WeakenedSoul:Exist(Friend) then
                 if smartRecast("PowerWordShield",Friend) then FiveSecondRuleTime = DMW.Time return true end
-            end
-            if Setting("Party - Renew") and Friend.HP < Setting("Party - Renew Percent") and not Buff.Renew:Exist(Friend) then
-                if smartRecast("Renew",Friend) then FiveSecondRuleTime = DMW.Time return true end
             end
         end
     end
 end
 
+--------------
+---DPS Code---
+--------------
 local function DPS()
+    --Shadow Word Pain Spread
     if Setting("Shadow Word: Pain") then
         for _, Unit in ipairs(Player40Y) do
             if Debuff.ShadowWordPain:Refresh(Unit) and (Unit.TTD - Debuff.ShadowWordPain:Remain(Unit)) > 4 or not Debuff.ShadowWordPain:Exist(Unit) then
@@ -88,7 +114,11 @@ local function DPS()
             end
         end
     end
-
+    --Holy Fire On Target
+    if Setting("Holy Fire") and Spell.HolyFire:IsReady() and Power > Setting("Mana Cut Off") and not Debuff.HolyFire:Exist(Target) then
+        if smartRecast("HolyFire",Target) then return true end
+    end
+    --Mind Blast Cast
     if Setting("Mind Blast") and not MeleeAggro and Power > Setting("Mana Cut Off") and Spell.MindBlast:IsReady() then
         if IsAutoRepeatSpell(Spell.Shoot.SpellName) then
             MoveForwardStart()
@@ -97,11 +127,11 @@ local function DPS()
         end
         if Spell.MindBlast:Cast(Target) then FiveSecondRuleTime = DMW.Time return true end
     end
-
+    --Smite Cast
     if Setting("Smite") and not MeleeAggro and Power > Setting("Mana Cut Off") then
         if Spell.Smite:Cast(Target) then FiveSecondRuleTime = DMW.Time return true end
     end
-
+    -- Auto Wand Management
     if not Player.Moving and not IsAutoRepeatSpell(Spell.Shoot.SpellName) and (DMW.Time - ShootTime) > 0.7 then
         if Spell.Shoot:Cast(Target) then
             ShootTime = DMW.Time
@@ -111,40 +141,70 @@ local function DPS()
 end
 
 local function DEF()
+    --Fortitude Self Check
     if not Buff.PowerWordFortitude:Exist(Player) then 
         if Spell.PowerWordFortitude:Cast(Player) then return true end
     end
-	if not Buff.InnerFire:Exist(Player) then 
+    --Inner Fire Self Check
+	if Setting("Auto Inner Fire") and not Buff.InnerFire:Exist(Player) then 
         if Spell.InnerFire:Cast(Player) then return true end
     end
+    --ShadowGuard Self Check
+    if Setting("Auto Shadowguard") and not Buff.Shadowguard:Exist(Player) then
+        if Spell.Shadowguard:Cast(Player) then return true end
+    end
+    --Defensive Renew
     if not Buff.Renew:Exist(Player) and Setting("Renew") and (HP <= Setting("Renew Percent") or (not Player.Combat and HP < 80)) and Power > 15 then
         if Spell.Renew:Cast(Player) then FiveSecondRuleTime = DMW.Time return true end
     end
+    --Defensive Lesser Heal
     if Setting("Use Lesser Heal") and HP < Setting("Heal Percent") and Power > 15 then
         if Spell.LesserHeal:Cast(Player) then return true end
     end
+    --Defensive Shield
     if Player.Combat and Setting("Power Word: Shield") and HP < Setting("Shield Percent") and Power > 30 and not Debuff.WeakenedSoul:Exist(Player) then
         if Spell.PowerWordShield:Cast(Player) then return true end
     end
 end
 
 function Priest.Rotation()
+    -- Init Locals
     Locals()
     if Rotation.Active() then
+        -----------------
+        --Out Of Combat--
+        -----------------
         if FiveSecond() then return true end
+        --Cast SWP on target, regardless of combat
         if Setting("Pull Spell") and Target and Target.ValidEnemy and not Debuff.ShadowWordPain:Exist(Target) then
             if smartRecast("ShadowWordPain",Target) then
                 return true
             end
         end
+        --Mind Blast Snipe (WIP)
+        for _, Unit in ipairs(Player40Y) do
+            if Setting("Mind Blast Snipe") and Unit.TTD <= Setting("Snipe TTD") and not Spell.MindBlast:LastCast() then
+                if Spell.MindBlast:Cast(Unit) then FiveSecondRuleTime = DMW.Time return end
+            end
+        end
+        -- Call Defensive Actionlist
         if DEF() then return true end
+        -- Call Healing Actionlist
         if HEAL() then return true end
+        -----------------
+        -----Combat------
+        -----------------
         if Player.Combat then
+            -- Auto attack if no wand is equipped
             if not DMW.Player.Equipment[18] and not IsCurrentSpell(Spell.Attack.SpellID) then
                 StartAttack()
             end
             if Setting("DPS Stuff") then
-                Player:AutoTarget(40, true)
+                -- Auto Target Enemy regardless of target
+                if HUD.TargetLock == 1 and UnitIsFriend(Target) then
+                    TargetLastEnemy()
+                end
+                -- Call DPS Actionlist
                 if DPS() then
                     return true
                 end
--- Settings.lua
+++ Settings.lua
@@ -6,6 +6,14 @@ local UI = DMW.UI
 
 
 function Priest.Settings()
+        UI.HUD.Options = {
+                [1] = {
+                    TargetLock = {
+                        [1] = {Text = "Enemy Target Lock |cFF00FF00On", Tooltip = ""},
+                        [2] = {Text = "Enemy Target Lock |cFFFFFF00Off", Tooltip = ""}
+                        }
+                    }
+                }
         UI.AddHeader("Party Healing")
         UI.AddToggle("Five Second Rule", "Set time to not break 5 second rule")
         UI.AddRange("Five Second Cutoff", "Set time to not break 5 second rule", 0, 5, 0.1, 4.5)
@@ -20,17 +28,25 @@ function Priest.Settings()
         UI.AddToggle("Party - Shield",nil)
         UI.AddRange("Party - Shield Percent", nil, 0, 100, 5 ,50)     
         UI.AddToggle("Party - Renew",nil)
-        UI.AddRange("Party - Renew Percent", nil, 0, 100, 5 ,50)      
+        UI.AddRange("Party - Renew Percent", nil, 0, 100, 5 ,50)
+        UI.AddBlank()
+        UI.AddRange("Renew Count Limit",nil,0,10,1,3)      
         UI.AddHeader("DPS")
         UI.AddToggle("DPS Stuff")
         UI.AddBlank()
         UI.AddToggle("Pull Spell", nil, 1)
         UI.AddToggle("Shadow Word: Pain", nil, 1)
         UI.AddToggle("Smite", nil, 1)
+        UI.AddToggle("Holy Fire", nil)
         UI.AddToggle("Mind Blast", nil, 1)
-        UI.AddToggle("Heal", nil, 1)
+        UI.AddBlank()
+        --UI.AddToggle("Heal", nil, 1)
+        UI.AddToggle("Mind Blast Snipe", nil)
+        UI.AddRange("Snipe TTD", nil, 0, 10, 0.1, 3)
         UI.AddRange("Mana Cut Off", "Mana Cut off for Smite and Mind blast", 0, 100, 1, 50)
         UI.AddHeader("Defensives")
+        UI.AddToggle("Auto Inner Fire", nil)
+        UI.AddToggle("Auto Shadowguard", nil)
         UI.AddToggle("Use Lesser Heal", nil, 1)
         UI.AddRange("Heal Percent", nil, 0, 100, 5, 40)
         UI.AddToggle("Power Word: Shield", nil, 1)
